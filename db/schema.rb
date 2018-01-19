c0905cdc0027b04fa8c4595830c06f03e1ce6dd6
Rita <rita.lu@yahoo.com.tw>
2018-01-19 08:17:23 +0800

    migration practice and build routes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180118000644) do
+ActiveRecord::Schema.define(version: 20180118131554) do
 
   create_table "photos", force: :cascade do |t|
     t.string "title"
